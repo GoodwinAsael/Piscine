ad12ce42cf67df236ed4e43b55a0016cf91519e7
GoodwinAsael <146069378+GoodwinAsael@users.noreply.github.com>
2023-10-02 18:31:18 +0200

    day2
    exercices 1-4

---
+++ Day 2/ex4.D2.js
@@ -0,0 +1,8 @@
+displayThisText (typeof 42)
+displayThisText (typeof xfcghvjbk)
+displayThisText (typeof "42,42")
+displayThisText (typeof $)
+displayThisText (typeof Symbol())
+displayThisText (typeof [1,2,3] )
+displayThisText (typeof 9854569n)
+displayThisText (typeof false)
